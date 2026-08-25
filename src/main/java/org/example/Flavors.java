a6e79a2d6ee987b89ca450b83691aa27bf1fe723
origal44 <origal44@gmail.com>
2026-08-25 16:23:00 +0300

    ex2 try 2

--- src/main/java/org/example/Flavors.java
+++ src/main/java/org/example/Flavors.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public enum Flavors {
+    chocolate(0),
+    vanilla(1),
+    oreos(2),
+    ferro(3),
+    strawberry(4);
+
+    private final int code;
+
+    private Flavors(int code) {
+        this.code = code;
+    }
+
+    public int GetCode() {
+        return this.code;
+    }
 }
--- src/main/java/org/example/IceCream.java
+++ src/main/java/org/example/IceCream.java
@@ -1,4 +1,49 @@
 package org.example;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class IceCream {
+    static void main() {
+        int[] array = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please choose a amount of balls");
+        int balls = scanner.nextInt();
+        int answer = -1;
+        if (balls > 5) {
+            System.out.println("you need a diet you fat nigero");
+            System.exit(0);
+        }
+
+        System.out.println("which of the following flavros would you like ");
+
+        for(Flavors flavor : Flavors.values()) {
+            System.out.print(flavor + " " + flavor.GetCode() + " ");
+        }
+
+        answer = scanner.nextInt();
+        int count = balls - 1;
+        int flag = 0;
+        if (answer > 4 || answer < 0) {
+            System.out.println("pls enter an actuall flavor");
+            ++count;
+            flag = 1;
+        }
+
+        if (flag == 0) {
+            int var10002 = array[answer]++;
+        }
+
+        while(count != 0) {
+            System.out.println("enter a flavor");
+            answer = scanner.nextInt();
+             array[answer]++;
+            --count;
+        }
+
+        for(int i = 0; i < balls; ++i) {
+            System.out.println(array[i]);
+        }
+
+    }
 }
