6c43887a2efbf91057b0bf7956b7c57b3580b979
origal44 <origal44@gmail.com>
2026-09-08 14:27:42 +0300

    sokoban with lombok

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,65 +1,36 @@
 package org.example;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@AllArgsConstructor
 public class Board {
     private TileType[][] grid;
     private Player player;
     private Set<Position> boxes;
     private Set<Position> targets;
+    @Setter
     private Policy policy;
-
-    public Board(TileType[][] grid, Player player, Set<Position> boxes,
-                 Set<Position> targets, Policy policy) {
-        this.grid = grid;
-        this.player = player;
-        this.boxes = boxes;
-        this.targets = targets;
-        this.policy = policy;
-    }
 
     public void move(Direction direction) {
         if (policy.isLegalMove(this, direction)) {
+            Position dest = new Position(player.getPosition().getX() + direction.getDx(),
+                    player.getPosition().getY() + direction.getDy());
 
-            Position dest = new Position(player.getPosition().getX() + direction.getDx(), player.getPosition().getY()+ direction.getDy());
-
-
-            if(boxes.contains(dest)) {
-                Position Ndest = new Position(dest.getX()+direction.getDx(), dest.getY()+direction.getDy());
+            if (boxes.contains(dest)) {
+                Position Ndest = new Position(dest.getX() + direction.getDx(),
+                        dest.getY() + direction.getDy());
                 boxes.remove(dest);
                 boxes.add(Ndest);
-
             }
             player.setPosition(dest);
-
         }
-
     }
 
-
-    public TileType[][] getGrid() {
-        return grid;
-    }
-
-    public Player getPlayer() {
-        return player;
-    }
-
-    public Set<Position> getBoxes() {
-        return boxes;
-    }
-
-    public Set<Position> getTargets() {
-        return targets;
-    }
-
-    public Policy getPolicy() {
-        return policy;
-    }
-
-    public void setPolicy(Policy policy) {
-        this.policy = policy;
-    }
     public void placeWall(Position pos) {
         grid[pos.getY()][pos.getX()] = TileType.WALL;
     }
@@ -79,6 +50,7 @@ public class Board {
     public void setPlayer(Position pos) {
         player.setPosition(pos);
     }
+
     public void initGrid(int width, int height) {
         grid = new TileType[height][width];
         for (int y = 0; y < height; y++)
--- src/main/java/org/example/Position.java
+++ src/main/java/org/example/Position.java
@@ -1,44 +1,15 @@
 package org.example;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
-// נקודה "טהורה" על הלוח - רק x,y.
-// שים לב: מתודת Move הוסרה מכאן! נקודה בודדת לא אחראית להזיז שחקן.
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Position {
     private int x;
     private int y;
-
-    public Position(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || !(o instanceof Position)) return false;
-        Position other = (Position) o;
-        return this.x == other.x && this.y == other.y;
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.x, this.y);
-    }
-
 }
