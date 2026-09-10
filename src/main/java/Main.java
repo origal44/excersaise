7d4d68e99800070f64d6a454392330f95a7d52ec
origal44 <origal44@gmail.com>
2026-09-10 10:10:13 +0300

    reformated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     static void main() {
         Scanner scanner = new Scanner(System.in);
@@ -9,15 +8,15 @@ public class Main {
         System.out.println("cola-1,colaZero-2,Sprite-3,fanta-4,fuzetea-5");
         int code = scanner.nextInt();
 
-        while(code>5 || code<1){
+        while (code>5 || code<1) {
             System.out.println("pls enter a valid number");
              code = scanner.nextInt();
 
         }
 
-            Drinks var = Drinks.fromCode(code);
+            Drinks ChosenDrink = Drinks.fromCode(code);
 
-            System.out.println("you chose : " + var);
+            System.out.println("you chose : " + ChosenDrink);
 
 
         }
